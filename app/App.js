3439778ee2fa645c63a4253665c7c4b25d7e06f4
Dorlin Shellen <dorlin.shellen@gmail.com>
2025-04-26 16:32:12 +0200

    setup front

---
+++ app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="app-name">
+        Hello World !
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: 'development',
+
+  entry: './app/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+	  filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+
+      // First Rule
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
+      // Second Rule
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  }
+};
